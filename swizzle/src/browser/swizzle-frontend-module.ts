6f4f5fe975f82907879913dc2c782c715af27fa6
Adam Barr-Neuwirth <6325943+slugiscool99@users.noreply.github.com>
2023-08-28 15:32:05 -0700

    push

--- swizzle/src/browser/swizzle-frontend-module.ts
+++ swizzle/src/browser/swizzle-frontend-module.ts
@@ -5,18 +5,11 @@ import { ContainerModule } from '@theia/core/shared/inversify';
 import { SwizzleContribution } from './swizzle-contribution';
 import { FrontendApplicationContribution } from '@theia/core/lib/browser';
 import { PluginVSCodeContribution } from '@theia/plugin-ext-vscode/lib/browser/plugin-vscode-contribution';
-// import { UserStorageContribution } from '@theia/userstorage/lib/browser/user-storage-contribution';
 
 export default new ContainerModule((bind, unbind) => {
     bind(SwizzleContribution).toSelf().inSingletonScope();
     bind(FrontendApplicationContribution).toService(SwizzleContribution);
-    
-    // bind(PluginVSCodeContribution).toSelf().inSingletonScope();
     bind(FrontendApplicationContribution).toService(PluginVSCodeContribution);
-
-    // bind(UserStorageContribution).toSelf().inSingletonScope();
-    // bind(FrontendApplicationContribution).toService(UserStorageContribution);
-
 
     bind(FrontendApplicationContribution).toDynamicValue(ctx => ({
         onStart(): void {
