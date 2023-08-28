c90ff0e83ed89e94214edb0204eea81f0555e1a8
Scott Bass <scott@galaxysdk.com>
2023-08-28 16:20:33 -0700

    Debug service

--- swizzle/src/browser/swizzle-frontend-module.ts
+++ swizzle/src/browser/swizzle-frontend-module.ts
@@ -5,14 +5,16 @@ import { ContainerModule } from '@theia/core/shared/inversify';
 import { SwizzleContribution } from './swizzle-contribution';
 import { FrontendApplicationContribution } from '@theia/core/lib/browser';
 import { PluginVSCodeContribution } from '@theia/plugin-ext-vscode/lib/browser/plugin-vscode-contribution';
+import { DebugService } from '@theia/debug/lib/common/debug-service';
 // import { UserStorageContribution } from '@theia/userstorage/lib/browser/user-storage-contribution';
 
 export default new ContainerModule((bind, unbind) => {
     bind(SwizzleContribution).toSelf().inSingletonScope();
     bind(FrontendApplicationContribution).toService(SwizzleContribution);
-    
+
     // bind(PluginVSCodeContribution).toSelf().inSingletonScope();
     bind(FrontendApplicationContribution).toService(PluginVSCodeContribution);
+    bind(FrontendApplicationContribution).toService(DebugService);
 
     // bind(UserStorageContribution).toSelf().inSingletonScope();
     // bind(FrontendApplicationContribution).toService(UserStorageContribution);
